d004276536499f0dca0f99f9476bf5ded90489cb
jeppung <jeppung@gmail.com>
2024-04-15 16:31:26 +0700

    add images validation

--- README.md
+++ README.md
@@ -4,10 +4,11 @@
 - [x] Make it deploy
 - [x] Scaffold basic ui with mock data
 - [x] Tidy up build process
-- [ ] Actually setup a database (vercel postgres)
-- [ ] Attach db to UI
-- [ ] Add authentication (w/ clerk)
+- [x] Actually setup a database (vercel postgres)
+- [x] Attach db to UI
+- [x] Add authentication (w/ clerk)
 - [ ] add image upload
+- [ ] "taint" (server-only)
 - [ ] Error management (w/ Sentry)
 - [ ] Routing/image page (parallel route)
 - [ ] Delete button (w/ Server Action)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,36 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { db } from "../server/db";
 
 export const dynamic = "force-dynamic";
 
-export default async function HomePage() {
+async function Images() {
   const images = await db.query.images.findMany({
     orderBy: (model, { desc }) => desc(model.id),
   });
 
   return (
+    <div className="flex flex-wrap gap-4">
+      {[...images, ...images, ...images].map((image) => (
+        <div key={image.id}>
+          <img src={image.url} className="w-48" />
+          <p>{image.name}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default async function HomePage() {
+  return (
     <main>
-      <div className="flex flex-wrap gap-4">
-        {[...images, ...images, ...images].map((image) => (
-          <div key={image.id}>
-            <img src={image.url} className="w-48" />
-            <p>{image.name}</p>
-          </div>
-        ))}
-      </div>
+      <SignedOut>
+        <div className="h-full w-full text-center text-2xl">
+          Please sign in above
+        </div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
