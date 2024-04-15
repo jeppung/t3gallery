7075b7b803a7f028905636f421eb864fe3598022
jeppung <jeppung@gmail.com>
2024-04-15 10:12:39 +0700

    DB Setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import { db } from "../server/db";
+
 const mockUrls = [
   "https://utfs.io/f/712b10e6-88a5-463a-b0cd-357e1aaf2804-hg4weo.png",
   "https://utfs.io/f/3abd2a70-82e8-44cf-b1a6-58b422b593fd-hcfzne.png",
@@ -10,10 +12,16 @@ const mockImages = mockUrls.map((url, i) => ({
   url,
 }));
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+
+  console.log(posts);
   return (
     <main>
       <div className="flex flex-wrap gap-4">
+        {posts.map((post) => (
+          <div key={post.id}>{post.name}</div>
+        ))}
         {[...mockImages, ...mockImages, ...mockImages].map((image) => (
           <div key={image.id}>
             <img src={image.url} className="w-48" />
